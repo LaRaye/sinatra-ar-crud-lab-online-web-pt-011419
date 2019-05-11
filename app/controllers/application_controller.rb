f9e3ab540588ca2fcc34a79f305413386814f3f5
LaRaye Johnson <lnj2105@gmail.com>
2019-05-11 20:13:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +7,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
   get '/' do
+    binding.pry
+  end 
+  
+  get '/articles/new' do
+    binding.pry
+    erb :new
+  end 
+  
+  post '/articles' do 
+    
+    @article = Article.create(params)
+    redirect '/articles'
   end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  attr_accessor :title, :content 
+  ARTICLES = []
+  
+  def initialize(params)
+    @title = params[:title]
+    @content = params[:content]
+    ARTICLES << self
+  end
+  
+  def self.all 
+    ARTICLES
+  end 
+  
+  def self.clear
+    ARTICLES.clear
+  end
 end
